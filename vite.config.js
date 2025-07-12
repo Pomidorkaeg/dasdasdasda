02a6d0f7651049189782258de35e48e0c19cd88f
Otvet_161 <hgfdh079@gmail.com>
2025-07-12 17:04:58 +0300

    fix: set correct base path for GitHub Pages in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -38,7 +38,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/hgfdhfh/',
+  base: '/sdf/',
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom'],
     esbuildOptions: {
